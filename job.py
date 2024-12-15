a011c7db428c255432a3313b2eb8c739598ae76b
civp <chiefpan0@gmail.com>
2024-12-15 19:39:05 +0100

    Fix job list maintenance issue

--- job.py
+++ job.py
@@ -1,3 +1,5 @@
+import heapq
+
 class Job(object):
     def __init__(self, name, thread):
         self.name = str(name)
@@ -16,15 +18,23 @@ class Job(object):
 class JobList(object):
     def __init__(self):
         self.jobs = {}
+        self.next_jid = 0
+        self.jid_pool = [] # Min heap
 
     def __repr__(self):
         return '\n'.join([f'[{jid}] {job}' for jid, job in self.jobs.items()])
 
     def add_job(self, job):
-        self.jobs[len(self.jobs)] = job
+        if self.jid_pool:
+            jid = heapq.heappop(self.jid_pool)
+        else:
+            jid  = self.next_jid
+            self.next_jid += 1
+        self.jobs[jid] = job
 
     def del_job(self, jid):
-        del self.jobs[jid]
+        if jid < self.next_jid and jid not in self.jid_pool:
+            heapq.heappush(self.jid_pool, jid)
 
     def check(self):
         jids_done = []
